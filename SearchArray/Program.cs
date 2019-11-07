732e95047bcfc7d39a264bc9e45b950981d4df0a
Goffigus <56737195+Goffigus@users.noreply.github.com>
2019-11-07 10:47:07 -0500

    Add project files.

---
+++ SearchArray/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using static System.Console;
+
+namespace SearchArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] listNames = { "Charlie", "Shar", "Kodos", "Henry", "Fred", "Gates", "Abby" };
+            int[] ages = { 5, 12, 24, 19, 12, 10, 13 };
+
+            int findAge = -1;
+            string inputName = ReadLine();
+
+            for(int i = 0; i < listNames.Length; ++i)
+            {
+                if(inputName == listNames[i])
+                {
+                    findAge = ages[i];
+                    i = listNames.Length;
+                }
+            }
+
+            if(findAge == -1)
+            {
+                WriteLine("Name does not exist");
+            } else
+            {
+                WriteLine("{0} age is: {1}", inputName, findAge);
+            }
+            
+        }
+    }
+}
